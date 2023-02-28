21b4da1f520a9a63bd3b312cd120bb82a959e13c
lord-charles <mwanikicharles226@gmail.com>
2023-02-28 08:23:11 +0300

    forth

--- controller/productCtl.js
+++ controller/productCtl.js
@@ -6,7 +6,7 @@ const validateMongodbId = require("../utils/validateMongodbId");
 
 const createProduct = asyncHandler(async (req, res) => {
   if (req.body.title) {
-    req.body.slug = slugify(req.body.title);
+    req.body.slug = slugify(`${req.body.title}${Date.now()}`);
   }
   let newProduct = await Product.create(req.body);
   res.status(200).json(newProduct);
